e153b4aab3105daa208606b327e2330cfec498f5
NickBarkerDev <nick.barker.devops@gmail.com>
2023-08-06 01:36:31 -0700

    added image to <Missing /> element

--- react-client/src/components/Missing.jsx
+++ react-client/src/components/Missing.jsx
@@ -6,7 +6,9 @@ const Missing = (props) => {
 
   return (
     <main>
-      <h1>Whoopsies, we can't find that page!</h1>
+      <h1>Whoopsies, I can't find that page!</h1>
+      <img className='page-missing-gif' src="https://media.tenor.com/RpghvJN-ucgAAAAC/sorry.gif" alt="Puppy looking sorry" />
+      <h4 className='wooked'>I wooked vewy hawd, I pwomise!</h4>
       <button onClick={() => {navigate(-1)}} className='btn btn-primary'>Back to previous page</button>
     </main>
   );
