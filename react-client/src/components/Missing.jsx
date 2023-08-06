0fd612fb3c9a4093752a449f40d7f50e41109c22
NickBarkerDev <nick.barker.devops@gmail.com>
2023-08-05 23:40:19 -0700

    Moved all shared components into /layouts & added back button to <Missing />

--- react-client/src/components/Missing.jsx
+++ react-client/src/components/Missing.jsx
@@ -1,13 +1,13 @@
 import { Link, useNavigate } from 'react-router-dom'
 
 const Missing = (props) => {
-  
+
   const navigate = useNavigate()
 
   return (
     <main>
-      <h1>Missing</h1>
-      <Link onClick={() => {navigate(-1, {replace: true})}}>Back to previous page</Link>
+      <h1>Whoopsies, we can't find that page!</h1>
+      <button onClick={() => {navigate(-1)}} className='btn btn-primary'>Back to previous page</button>
     </main>
   );
 }
--- react-client/src/components/layouts/Home.jsx
+++ react-client/src/components/layouts/Home.jsx
@@ -1,6 +1,6 @@
-import Header from "../Header"
-import Nav from "../Nav"
-import Footer from "../Footer"
+import Header from "./Header"
+import Nav from "./Nav"
+import Footer from "./Footer"
 import { Outlet } from 'react-router-dom'
 
 const Home = (props) => {
