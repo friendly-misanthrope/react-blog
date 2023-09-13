693dd04b0725bbc7a65dcea49a4a98c9f96ae91f
NickBarkerDev <nick.barker.devops@gmail.com>
2023-09-13 01:15:09 -0700

    cleaned up useContext declaration in Home.jsx to include only the needed data

--- react-client/src/components/layouts/Home.jsx
+++ react-client/src/components/layouts/Home.jsx
@@ -2,22 +2,19 @@ import Header from "./Header"
 import Nav from "./Nav"
 import Footer from "./Footer"
 import Dashboard from "../Dashboard"
-import { useLocation, Outlet } from 'react-router-dom'
-import useWindowSize from '../../hooks/useWindowSize'
+import { Outlet } from 'react-router-dom'
 import DataContext from '../../context/DataContext'
 import { useContext } from 'react'
 
 
 const Home = () => {
-  const { width } = useWindowSize()
-  const location = useLocation()
 
-  const { search, setSearch, posts, fetchError, isLoading } = useContext(DataContext)
+  const { location } = useContext(DataContext)
 
   return (
     <main className="home">
-      <Header width={width} />
-      <Nav search={search} setSearch={setSearch} />
+      <Header />
+      <Nav />
   
       {
         location.pathname === '/' ?
