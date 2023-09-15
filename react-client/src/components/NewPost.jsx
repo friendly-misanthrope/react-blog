1b45385292b50ed00757b0a011f297e5c2a06f03
NickBarkerDev <nick.barker.devops@gmail.com>
2023-09-15 03:09:25 -0700

    created some new posts

--- react-client/src/components/NewPost.jsx
+++ react-client/src/components/NewPost.jsx
@@ -11,10 +11,10 @@ const NewPost = () => {
       <form onSubmit={handleNewPostSubmit} method="post" className="new-post-form">
         
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" maxLength={42} onChange={handleNewPostChange} value={newPost.title} autoFocus="autofocus" required />
+        <input type="text" id="title" name="title" maxLength={30} onChange={handleNewPostChange} value={newPost.title} autoFocus="autofocus" required />
 
         <label htmlFor="body">Post:</label>
-        <textarea name="body" id="body" cols="40" rows="6" maxLength={280} onChange={handleNewPostChange} value={newPost.body} wrap="hard" required></textarea>
+        <textarea name="body" id="body" cols="40" rows="6" maxLength={320} onChange={handleNewPostChange} value={newPost.body} wrap="hard" required></textarea>
 
         <button type="submit" className='btn btn-primary'>Post</button>
       </form>
